0139a8c790cd0a08c45f9006886b89ac79b0ea9c
Sebastián Escobar <Sebastian.escobarm@upb.edu.co>
2025-10-28 14:35:01 -0500

    Videoplayer fix

--- Virus LN/Assets/Scripts/BotonDePagina.cs
+++ Virus LN/Assets/Scripts/BotonDePagina.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class BotonDePagina : MonoBehaviour
 {
     public MoveData data;
     public int seccionACargar;
+    public UnityEvent OnClickEventoptional;
     private void OnMouseDown()
     {
         MoveToPlace.instance.MoveCameraToDataPlace(data);
         PageManager.instance.CloseAll();
         Manager.instance.ActivarSeccion(seccionACargar);
+        OnClickEventoptional.Invoke();
     }
 
     public void OnBackButton()
---
+++ Virus LN/Assets/WebGLVideoTest.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.Video;
+
+public class WebGLVideoFix : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+
+    void Start()
+    {
+#if UNITY_WEBGL && !UNITY_EDITOR
+        // En WebGL, forzar la carga completa antes de reproducir
+        videoPlayer.Prepare();
+        videoPlayer.prepareCompleted += (v) =>
+        {
+            v.time = 0;
+            v.Play();
+        };
+#else
+        videoPlayer.time = 0;
+#endif
+    }
+
+    public void Replay()
+    {
+        videoPlayer.Stop();
+        videoPlayer.time = 0;
+        videoPlayer.Play();
+    }
+}
